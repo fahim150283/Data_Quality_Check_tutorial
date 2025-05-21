1b676cca36a19e3d0a4753b6b9ee74775e31defd
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-05-21 17:50:45 +0600

    python variables is done

---
+++ Basics/PythonSyntax.py
@@ -0,0 +1,13 @@
+print("This is about Python Syntax")
+
+x = 5
+thief = "John"
+print(x)
+print(thief)
+
+
+# syntax is different from java
+if x > 2:
+    print(x,"is greater than two")
+else:
+    print(x,"is not greater than two")
---
+++ Basics/PythonVariable.py
@@ -0,0 +1,30 @@
+x = str(3)+"jhakanaka"
+y = int(3)
+z = float(3.0002)
+
+print(y+z)
+print("hi" ,3)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+
+
+x=4
+y=11
+print(x,y)
+print(x+y)
+
+def add(x,y):
+    return y-x
+
+print(add(x,y))
