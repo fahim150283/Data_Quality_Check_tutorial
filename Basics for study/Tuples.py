805bced6b5dc45df7fa8f1b6b16012531a0f6aee
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-05-26 17:57:13 +0600

    the tuple is in progress

--- Basics for study/Tuples.py
+++ Basics for study/Tuples.py
@@ -1,3 +1,23 @@
-mytuple = ("apple", "banana", "cherry")
-print(mytuple)
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print("1.",thistuple)
+print("2.",len(thistuple))
 
+thistuple = ("apple",)
+print("3.",type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print("4.",type(thistuple))
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+print("5.",tuple1)
+print("6.",tuple2)
+print("7.",tuple3)
+
+thistuple = ("apple", "banana", "cherry")
+print("8.",len(thistuple))
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print("9.",thistuple)
