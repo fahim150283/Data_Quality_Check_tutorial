101cd4ff873698052f5309a81a2aef2b5fdb0fdc
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-05-27 16:25:59 +0600

    match case is complete

---
+++ Basics for study/Match Case.py
@@ -0,0 +1,37 @@
+print("1.")
+day = 3
+
+
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
+  case _:
+    print("Invalid day")
+
+
+
+
+
+
+
+print("2.")
+day = 6
+match day:
+  case 1 | 2 | 3 | 4 | 5:
+    print("Today is a weekday")
+  case 6 | 7:
+    print("I love weekends!")
+  case _:
+    print("Invalid day")
