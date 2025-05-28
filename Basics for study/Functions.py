2456eda00fa8e9975bc15657eaf5d123bcf5b28f
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-05-28 16:35:16 +0600

    the function is done

--- Basics for study/Functions.py
+++ Basics for study/Functions.py
@@ -1,12 +1,54 @@
-def my_function(fname):
-  print(fname)
+def func1(fname):
+  print("\t" + fname)
+
+def func2(fname, lname):
+  print("\t" + fname + " " + lname)
+
+def func3(*kids):
+  print("\t","The youngest child is " + kids[len(kids)-1])
+  print("\t","The oldest child is " + kids[0])
+  print("\t","The middle child is " + kids[len(kids)//2])
+
+def func4(country ="Norway"):
+  print("\t","I am from " + country)
+
+def func5(fruits):
+  for x in fruits:
+    print("\t",x)
+
+print("1.")
+(func2("Will", "Smith"))
+
+print("2.")
+func1("Emil")
+func1("Tobias")
+func1("Linus")
+
+
+print("3.")
+func3("Emil", "Tobias", "Linus", "Will", "Smith")
+
+
+print("4.")
+func4("Sweden")
+func4("India")
+func4()
+func4("Brazil")
+
+
+print("5.")
+fruits = ["apple", "banana", "cherry"]
+func5(fruits)
 
 
 
-def my_function(fname, lname):
-  print(fname + " " + lname)
 
-my_function("Emil", "Refsnes")
-my_function("Emil")
-my_function("Tobias")
-my_function("Linus")
+print("6. Recursion")
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print("\t",result)
+  else:
+    result = 0
+  return result
+tri_recursion(6)
