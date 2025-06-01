88f54fd9f678dd7e898491631aff4bacb1ee217a
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-06-01 20:54:49 +0600

    the inheritance is still to be covered

--- Basics for study/OOPs/Inheritance.py
+++ Basics for study/OOPs/Inheritance.py
@@ -1,13 +1,20 @@
 class Parent:
-    def __init__(self,a,b):
+    def __init__(self, a, b):
         print("Parent Constructor")
-        print(a,b)
+        print(a, b)
 
     def m1(self):
         print("Parent Method")
 
 class Child(Parent):
-    def haha(self):
-        print("Child Constructor")
-        super().__init__(10,20)
-        super().m1()
+    def child(self):
+        print("Child Method")
+        super().__init__(10, 20)  # Calls Parent.__init__
+        super().m1()  # Calls Parent.m1
+
+    def haha(self):  # Define the missing method
+        print("Haha method called!")
+
+# Usage:
+c = Child(5, 6)  # Must pass (a, b) since Parent.__init__ needs them
+c.haha()  # Now works!
