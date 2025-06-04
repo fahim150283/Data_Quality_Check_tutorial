a8fd7f7f38af9c1810985ce2657edf2e4028a48c
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-06-04 15:59:16 +0600

    Signing off now for Eid vacation. Can not concentrate xD

---
+++ Basics for study/OOPs/Polymorphism/Access file.py
@@ -0,0 +1,10 @@
+import Car as car
+import Plane as plane
+import Boat as boat
+import Vehicle as vehicle
+
+car1 = car.Car("Honda", "Civic", 2019)
+plane1 = plane.Plane("Boeing", "747", 2020)
+boat1 = boat.Boat("Honda", "Civic", 2019)
+
+vehicle1 = vehicle.Vehicle("Tank", "Tiger", 1945)
--- Basics for study/OOPs/Polymorphism/Vehicle.py
+++ Basics for study/OOPs/Polymorphism/Vehicle.py
@@ -1,7 +1,10 @@
 class Vehicle:
     def __init__(self, make, model, year):
-        self.vehicleType = "Vehicle"
+        vehicleType = "Vehicle"
         self.make = make
         self.model = model
         self.year = year
 
+
+    def move(self):
+        print("Drive!")
